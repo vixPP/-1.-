10e8bf5443229dbf83b7ad0a7a6d7b76925c0e79
vix <churink9@gmail.com>
2024-03-16 18:28:21 +0300

    first commit

---
+++ 1. Фигуры. Количество сторон.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+class Figure 
+{
+    
+protected:
+    int sides_count;
+
+public:
+    Figure():sides_count(0){}
+    int get_sides_counts() const 
+    {
+        return sides_count;
+    }
+};
+
+class Triangle : public Figure 
+{
+public:
+    Triangle()
+    {
+        sides_count = 3;
+    }
+   
+};
+
+class Quadrilateral : public Figure 
+{
+public:
+    Quadrilateral()
+    {
+        sides_count = 4;
+    }
+   
+};
+
+int main() 
+{
+    Figure figure;
+    Triangle triangle;
+    Quadrilateral quadrilateral;
+    cout << "Колличество сторон: " << endl;
+    cout << "Фигура: " << figure.get_sides_counts() << endl;
+    cout << "Треугольник: " << triangle.get_sides_counts() << endl;
+    cout << "Четырехугольник: " << quadrilateral.get_sides_counts() << endl;
+    
+    return 0;
+}
+
